b1c4d6bb6ea68365cbad23027d3191cb85d82322
natalia <natalia@mikamai.com>
2020-12-09 17:50:47 +0100

    day9_2

--- day9.js
+++ day9.js
@@ -1,8 +1,8 @@
 const day9 = () => {
-    const input = day9input.split(`\n`)
+    const input = day9input.split(`\n`).map(el => parseInt(el))
     const preambleNumber = 25
     
-    const preamble = (index) => input.slice(index - preambleNumber, index).map(el => parseInt(el))
+    const preamble = (index) => input.slice(index - preambleNumber, index)
     
     const findSumElementsByIndex = (elemIndex) => {
         let arr = []
@@ -17,12 +17,44 @@ const day9 = () => {
         })
         return [...new Set(arr)]
     }
-    const part1 = () => {
+
+    const findWeak = () => {
+        let found
         input.forEach((el, index) => {
-            if (findSumElementsByIndex(index).length < 1 && index >= preambleNumber) console.log(el)
-        })   
+            if (findSumElementsByIndex(index).length < 1 && index >= preambleNumber) found = el
+        })
+        return found
+    }
+
+    const part1 = () => {
+        console.log('day9_1: ', findWeak())
+    }
+
+    const part2 = () => {
+        const weak = findWeak()
+        let foundSet
+
+        const sumOfArray = (arr) => {
+            const reducer = (accumulator, currentValue) => accumulator + currentValue;
+            return arr.reduce(reducer)
+        }
+        
+        input.forEach((element, index) => {
+            for (let len = 2; len <= input.length - index; len++) {
+                let arr = input.slice(index, index + len)
+                if (sumOfArray(arr) === weak) {
+                    foundSet = arr
+                }
+            }
+        })
+
+        let min = foundSet.sort((a, b) => a - b)[0]
+        let max = foundSet.sort((a, b) => a - b)[foundSet.length - 1]
+
+        console.log('day9_2: ', min + max)
     }
     part1()
+    part2()
 }
 
 day9()
