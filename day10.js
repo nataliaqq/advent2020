1f7bfafe02a4ce21b6f8764b773a2bcd92de27e6
natalia <natalia@mikamai.com>
2020-12-11 14:31:49 +0100

    day10_2
    day10 2

--- day10.js
+++ day10.js
@@ -9,6 +9,23 @@ const day10 = () => {
     });
     console.log('day10_1: ', obj['1'].length * obj['3'].length)
 
+    // part2
+
+
+    let count = 0
+    let seq = []
+    inputSorted.forEach((el, index) => {    
+        let next = inputSorted[index + 1]
+        if (next) seq.push(next - el)
+    })
+
+    seq = seq.toString().replaceAll(',','')
+    seq = seq.replaceAll( '1111', '7' ).replaceAll( '111', '4' ).replaceAll( '11', '2' )
+    seq = seq.replaceAll( '3', '' ).replaceAll( '1','' )
+    
+    const v = seq.split('').map(x => parseInt(x)).reduce( (a, b) => a * b )
+    console.log('day10_2: ', v)
+
 }
 
 day10()
